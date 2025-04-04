887ffdbc978cb2ea68005cecb98e23efcabbcdfb
Spyke Lionel <social.lionel@gmail.com>
2025-04-04 18:51:54 +0100

    update settings

--- pportal/settings.py
+++ pportal/settings.py
@@ -23,7 +23,7 @@ import os
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # untechforcameroon.onrender.com
 ALLOWED_HOSTS = ['*']
 
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
